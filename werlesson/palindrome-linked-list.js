95d75bd3e37480af55a9cb07910d6facdec48037
Werlesson <werlessono@gmail.com>
2024-01-20 14:49:58 -0300

    Create palindrome-linked-list.js

---
+++ werlesson/palindrome-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function linkedListToArray(head) {
+    const result = [];
+    let current = head;
+
+    while (current !== null) {
+        result.push(current.val);
+        current = current.next;
+    }
+
+    return result;
+}
+
+var isPalindrome = function(head) {
+    return JSON.stringify(linkedListToArray(head)) === JSON.stringify(linkedListToArray(head).reverse())
+};
